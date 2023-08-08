b774c001a3f4b2ff3262ea5800ccd500777d71e5
Rairocha <52248306+Rairocha@users.noreply.github.com>
2023-08-08 17:11:40 -0300

    follow working withoust front end

--- src/components/PoliticianPreview.jsx
+++ src/components/PoliticianPreview.jsx
@@ -1,18 +1,28 @@
+import { useContext, useEffect} from "react"
 import { Link } from "react-router-dom";
 import { get, post } from "../services/authService"
+import { AuthContext } from "../context/auth.context"
 
-const PoliticianPreview = ({ pol }) => {
+const PoliticianPreview = ({ pol , follower, setFollow }) => {
   
+
   const newName= [pol.short_title,pol.first_name,pol.middle_name,pol.last_name].reduce((acc,i)=>{if(i){return acc+' '+i} else{return acc}})
 ;
+  const {  user } = useContext(AuthContext)
 
-  const addToUser=(polId)=>{
-    console.log(polId)
-    /*post(`/politicians/follow/${polId}`)
-    .then(()=>{})
-    .catch((err)=>{console.log(NativeError)})*/
+
+  
+
+  const addToUser=(polId,user)=>{
+    post(`/users/follow/${polId}`,user)
+    .then((updatedUser)=>{
+      setFollow(updatedUser.data.following);
+    }) 
+    .catch((err)=>{console.log(err)});
+    
   }
 
+  
 
   return (
 
@@ -20,10 +30,8 @@ const PoliticianPreview = ({ pol }) => {
     <Link to={`/politicians/${pol._id}`}>
           
             <p>{`${newName}`}</p>
-            <button onClick={(pol)=>{addToUser(pol._id)}}>Follow</button>
-          
-          
       </Link>
+      <button onClick={()=>{addToUser(pol._id,user)}}>Follow</button>
     </div>
         
 
--- src/pages/AllPoliticians.jsx
+++ src/pages/AllPoliticians.jsx
@@ -1,19 +1,28 @@
-import { useContext, useEffect } from "react"
+import { useContext, useEffect,useState } from "react"
 import { PoliticianContext } from "../context/politicians.context"
+import { AuthContext } from "../context/auth.context"
 import { Link } from "react-router-dom"
+import { get, post } from "../services/authService"
 
 import PoliticianPreview from "../components/PoliticianPreview"
 
 
 const AllPoliticians = () => {
+    const [follower, setFollow] = useState([]);
 
     const { pol, getPol } = useContext(PoliticianContext)
+    const {  user } = useContext(AuthContext)
 
     useEffect(() => {
-
-        if (!pol.length) {
-            getPol()
-        }
+        getPol();
+        get('/users/following',user)
+        .then((f)=>{
+          console.log('following get',f.data.following)
+          setFollow(f.data.following)
+        })
+        .catch((err) => {
+          console.log(err)
+       })
 
     }, [])
 
@@ -24,7 +33,7 @@ const AllPoliticians = () => {
             pol.map((p) => {
                 return (
                     
-                    <PoliticianPreview key={p._id} pol={p} />
+                    <PoliticianPreview key={p._id} pol={p} follower={follower} setFollow={setFollow}/>
 
                 )
             })
